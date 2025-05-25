61c691754b1c884f2cef169d9e253a0ca4001539
Nagesha S <43410978+NageshaSriram@users.noreply.github.com>
2025-05-25 19:49:49 +0530

    zomato create tables

--- zomato.sql
+++ zomato.sql
@@ -10,3 +10,18 @@ create table users (
     email varchar(50),
     address varchar(100)
 );
+
+create table restaurents (
+	rest_id int AUTO_INCREMENT primary key,
+    name varchar(50),
+    address varchar(100)
+);
+
+create table food (
+	food_id int AUTO_INCREMENT PRIMARY key,
+    food_name varchar(30)
+);
+
+alter table food add column rest_id int;
+
+alter table food add constraint fk_rest_id FOREIGN KEY (rest_id) references restaurents(rest_id);
